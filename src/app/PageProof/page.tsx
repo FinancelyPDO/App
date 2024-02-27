ca2f058f44c93d12598a490fceb681aef4828e83
mathis sergent <sergent.mathis@gmail.com>
2024-02-26 17:19:41 -0700

    Bank connection, fetch transactions and environment variables
    - Bank connection from the front end using Powens technical solution
    - Exchange "code" for an access token
    - Get transactions using the access token
    - Next environment variable have to be called NEXT_PUBLIC_ behind

--- src/app/PageProof/page.tsx
+++ src/app/PageProof/page.tsx
@@ -1,27 +1,24 @@
 'use client'
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSearchParams } from 'next/navigation';
 
 export default function PageProof() {
   const searchParams = useSearchParams();
+  const [accessToken, setAccessToken] = useState();
 
   const code = searchParams.get('code');
   const connection_id = searchParams.get('connection_id');
 
-  // verification
   console.log("code:" + code);
   console.log("connection_id: " + connection_id);
 
   useEffect(() => {
     if (code && connection_id) {
-      const clientId = process.env.CLIENT_ID;
-      const clientSecret = process.env.CLIENT_SECRET;
-
       const data = {
         code: code,
-        client_id: clientId,
-        client_secret: clientSecret
+        client_id: process.env.NEXT_PUBLIC_CLIENT_ID,
+        client_secret: process.env.NEXT_PUBLIC_CLIENT_SECRET
       };
 
       fetch(`https://myfreetest-sandbox.biapi.pro/2.0/auth/token/access`, {
@@ -35,12 +32,38 @@ export default function PageProof() {
       .then(data => {
         console.log(data);
         console.log('Access Token:', data.access_token);
+        setAccessToken(data.access_token);
       })
       .catch(error => {
         console.error('Error:', error);
       });
     }
   }, [code, connection_id]);
+
+  function getAccountData() {
+    if (code && connection_id) {
+      const data = {
+        code: code,
+        client_id: "10916951",
+        client_secret: "WfvrhN5i_5u3n6dJBRoWj70pIkCfkgQL"
+      };
+
+      fetch(`https://myfreetest-sandbox.biapi.pro/2.0/users/me/transactions?limit=1000`, {
+        method: 'GET',
+        headers: {
+          'Authorization': 'Bearer ' + accessToken,
+          'Content-Type': 'application/json'
+        },
+      })
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });
+    }
+  }
 
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
@@ -55,6 +78,7 @@ export default function PageProof() {
           <a href="/path/to/pdf-file.pdf" download className="block bg-indigo-500 text-white px-6 py-3 rounded-md text-center hover:bg-indigo-600 text-lg font-semibold">Download</a>
         </div>
       </div>
+      <button onClick={getAccountData}>Get data</button>
     </div>
   );
 }
