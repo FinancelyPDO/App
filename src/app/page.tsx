6c8dc490802bcb34837081f01bb975367069c66b
MarcBTHT <marc.berthol29@gmail.com>
2024-02-28 07:06:01 +0100

    style: :children_crossing: Home page modification
    Add Icon and color / text size modification

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,6 +19,24 @@ export default function Home() {
                 height: '550px',
               }}
             />
+            <div
+              className="absolute left-1/2 transform translate-x-40 -translate-y-80"
+              style={{
+                background: 'linear-gradient(to bottom, rgba(135,203,208,1) 20%, rgba(0,0,0,1) 90%)',
+                filter: 'blur(80px)',
+                width: '330px',
+                height: '330px',
+              }}
+            />
+            <div
+              className="absolute transform -translate-x-10 -translate-y-80 "
+              style={{
+                background: 'linear-gradient(to bottom, rgba(83,32,73,1) 30%, rgba(82,55,149,1) 93%)',
+                filter: 'blur(80px)',
+                width: '330px',
+                height: '330px',
+              }}
+            />
             <div className='relative z-10 mx-auto w-2/3'>
               <h1 className="text-8xl text-lavender font-sans font-bold leading-tight text-center responsive-heading">
                 Building Trust <br /> <span className="text-white">With Privacy.</span>
@@ -27,53 +45,61 @@ export default function Home() {
                 Securely verify and validate financial integrity without ever compromising privacy with our Private Data Oracle services.
               </p>
               <nav className="flex justify-center mt-20 p-8">
-                <Link href="/" className="text-2xl text-black mr-16 bg-white hover:bg-lavender font-bold font-sans py-6 px-14 rounded-lg transition duration-300">
+                <Link href="/" className="text-2xl text-black mr-16 bg-white hover:bg-lavender font-bold font-sans py-4 px-14 rounded-lg transition duration-300">
                   Discover
                 </Link>
-                <Link href="/" className="text-xl text-white font-bold py-6 px-4">
+                <Link href="/" className="text-xl text-white font-bold py-4 px-4">
                   Talk to an expert
                 </Link>
               </nav>
             </div>
           </section>
           {/* Section 3 */}
-          <section className=" w-full" style={{ borderTop: '1px solid black', borderBottom: '1px solid black' }}>
-            <div className="flex flex-wrap items-center justify-around mx-auto px-4 h-[720px]">
-              <div className="w-full md:w-1/2 flex justify-center"
-                style={{
-                  background: 'linear-gradient(to bottom, rgba(201, 117, 156, 0.7) 40%, rgba(212, 137, 127, 0.5) 50%)',
+          <section className=" w-full">
+            <div className="flex flex-wrap items-center justify-around mx-auto mt-20 px-4">
+              <div className="w-full md:w-1/2 flex justify-center relative">
+                <div className="absolute transform translate-x-10 " style={{
+                  background: 'linear-gradient(to bottom, rgba(167,251,255,1) 39%, rgba(0,0,0,0) 100%)',
                   filter: 'blur(180px)',
                   width: '550px',
                   height: '550px',
-                }}>
+                }}/>
                 <Image
-                  src="/images/icon/github.svg"
+                  src="/images/icon/alert-octagon.svg"
                   alt="Description of the first image"
-                  width={600}
+                  width={400}
                   height={400}
+                  className='z-5'
+                />
+                <Image
+                  src="/images/icon/shield2.svg"
+                  alt="Description of the first image"
+                  width={150}
+                  height={150}
+                  className='absolute z-10 transform translate-x-28 translate-y-72'
                 />
               </div>
               <div className="w-full md:w-1/2 mt-4 md:mt-0 px-20">
-                <h2 className="text-6xl font-tiempos-headline text-white mb-4">Proof of Integrity, Delivered.</h2>
-                <p className="text-gray-500 mb-8">
+                <h2 className="text-6xl font-bold font-tiempos-headline text-white mb-6">Proof of Integrity, Delivered.</h2>
+                <p className="text-gray-500 mb-8 text-xl">
                   Verify asset holdings in real-time, ensuring transparency and trust in financial declarations.
                 </p>
                 <Link href="/page-amount"
-                  className="inline-block bg-white ml-5 text-black hover:bg-old_rose px-4 py-2 rounded-lg transition duration-300">Dive In
+                  className="inline-block bg-white ml-5 text-black hover:bg-old_rose py-3 px-10 rounded-lg transition duration-300">Dive In
                 </Link>
               </div>
             </div>
           </section>
           {/* Section 4 */}
-          <section className=" w-full" style={{ borderBottom: '1px solid black' }}>
+          <section className="w-full mt-20">
             <div className="flex flex-wrap items-center justify-around mx-auto px-4 h-[720px]">
               <div className="w-full md:w-1/2 mt-4 md:mt-0 px-20">
-                <h2 className="text-6xl font-tiempos-headline text-white mb-4">Trust Index Score.</h2>
-                <p className="text-gray-500 mb-8">
+                <h2 className="text-6xl font-bold font-tiempos-headline text-white mb-6">Trust Index Score.</h2>
+                <p className="text-gray-500 mb-8 text-xl">
                   Leverage our proprietary Trust Index to quantify and demonstrate your credibility to partners and customers.
                 </p>
                 <Link href="/"
-                  className="inline-block bg-white ml-5 text-black hover:bg-old_rose px-4 py-2 rounded-lg transition duration-300">Dive In
+                  className="inline-block bg-white ml-5 text-black hover:bg-old_rose py-3 px-10 rounded-lg transition duration-300">Dive In
                 </Link>
               </div>
               <div className="w-full md:w-1/2 flex justify-center"
@@ -93,7 +119,7 @@ export default function Home() {
             </div>
           </section>
           {/* Section 5 */}
-          <section className="w-full" style={{ borderTop: '1px solid black', borderBottom: '1px solid black' }}>
+          <section className="w-full mt-20">
             <div className="flex flex-wrap items-center justify-around mx-auto px-4 h-[720px]">
               <div className="w-full md:w-1/2 flex justify-center"
                 style={{
@@ -110,12 +136,12 @@ export default function Home() {
                 />
               </div>
               <div className="w-full md:w-1/2 mt-4 md:mt-0 px-20">
-                <h2 className="text-6xl font-tiempos-headline text-white mb-4">Swap Solutions, Tailored for Foundations.</h2>
-                <p className="text-gray-500 mb-8">
+                <h2 className="text-6xl font-bold font-tiempos-headline text-white mb-6">Swap Solutions, Tailored for Foundations.</h2>
+                <p className="text-gray-500 mb-8 text-xl">
                   Provide your supporters with a secure, transparent means to contribute to your cause or project, bolstering trust through clear financial transactions.
                 </p>
                 <Link href="/"
-                  className="inline-block bg-white ml-5 text-black hover:bg-old_rose px-4 py-2 rounded-lg transition duration-300">Dive In
+                  className="inline-block bg-white ml-5 text-black hover:bg-old_rose py-3 px-10 rounded-lg transition duration-300">Dive In
                 </Link>
               </div>
             </div>
