07df2114316603c34f7a5e8792eb8033f776422c
MarcBTHT <marc.berthol29@gmail.com>
2024-03-01 10:41:54 +0100

    refactor: :art: all-proofs page select button

---
+++ src/components/constants/listProofs.tsx
@@ -0,0 +1,7 @@
+export const ListProofs = [
+    {label: "Reserve", value: "reserve", description: "Connect your bank account to the app to prove you have a certain amount on it."},
+    {label: "Transaction", value: "transaction", description: "Connect your bank account to the app to prove you have a certain amount on it."},
+    {label: "Insurance", value: "insurance", description: "Connect your bank account to the app to prove you have a certain amount on it."},
+    {label: "Fidelities", value: "fidelities", description: "Connect your bank account to the app to prove you have a certain amount on it."},
+  ];
+  
